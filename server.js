4cda726d3a4b140325a8d3c15cbe845a4b0d7d5a
Filippo Massarelli <filippo.massarelli@gmail.com>
2020-08-03 19:47:26 +0200

    Looooads of progress since last commit: Functionality done, including chained promises, post rout and dynamic updating of the DOM. Styling started to mimic my-projects react-weather-app

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
@@ -31,16 +33,65 @@ function listening() {
   console.log(`running on localhost: ${port}`);
 }
 
-// GET route
-app.get("/", sendData);
+// ROUTES
 
-function sendData(req, res) {
+//POST route
+app.post("/projectData", addData);
+
+function addData(req, res) {
+  console.log(req.body);
+  const newEntry = {
+    city: req.body.name,
+    temp: req.body.main.temp,
+    icon: req.body.weather[0].icon,
+    description: req.body.weather[0].description,
+    feeling: req.body.feeling,
+    date: req.body.date,
+  };
+  projectData = { ...newEntry };
+  console.log(
+    "search data posted to /searchData route. projectData = ",
+    projectData
+  );
   res.send(projectData);
 }
 
-// POST route
-app.post("/search", callBack);
+// GET route
+app.get("/projectData", getData);
 
-function callBack(req, res) {
-  res.send("search request received");
+function getData(req, res) {
+  res.status(200).send(projectData);
 }
+
+// API calls
+//in order to ensure API keys are kept stored as enviroment variables we need to run this server side (because dotenv is a node package)
+
+// const baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+// const units = "&units=metric";
+// const apiKey = "&appid=" + process.env.API_KEY;
+
+// const getWeather = async (baseUrl, units, key, city) => {
+//   const response = await fetch(baseUrl + city + key + units);
+
+//   try {
+//     const data = await response.json();
+//     console.log("getWeather successful", data);
+//     return data;
+//   } catch (error) {
+//     console.log("error", error);
+//   }
+// };
+
+// POST route for API call
+
+// app.get("/api", (req, res) => {
+//   console.log("hey ", req.body);
+
+//   const city = req.body;
+//   const data = getWeather(baseUrl, units, apiKey, city);
+//   console.log(
+//     "back end is communicating with client after successful api call",
+//     data
+//   );
+//   res.send(data);
+// }
--- website/app.js
+++ website/app.js
@@ -1,5 +1,137 @@
-/* Global Variables */
+// ------------
+// Event Listeners
+// both hitting enter after typing in the city, and clicking the button shpould trigger the same action
+
+document.getElementById("generate").addEventListener("click", handleClick);
+
+document.getElementById("city").addEventListener("keyup", function (e) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (e.keyCode === 13) {
+    // Trigger the button element with a click
+    document.getElementById("generate").click();
+  }
+});
+
+document.getElementById("feelings").addEventListener("keyup", function (e) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (e.keyCode === 13) {
+    // Trigger the button element with a click
+    document.getElementById("generate").click();
+  }
+});
+
+// ------------
+// Handle Event
+// using chained promises we respond to the user search by fetching data from the openweather api, storing it to server, and using it to update the user interface
 
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+function handleClick(e) {
+  const userCity =
+    document.getElementById("city").value === ""
+      ? "London"
+      : document.getElementById("city").value;
+
+  getWeather(baseUrl, units, apiKey, userCity)
+    // .then((data) => addFeeling(data))
+    .then((data) => {
+      let d = new Date();
+      let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+      postData("/projectData", {
+        ...data,
+        feeling: document.getElementById("feelings").value,
+        date: newDate,
+      });
+    })
+    .then(() => updateUI());
+}
+
+// Define helper functions
+
+//1. Fetch weather data from API
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+const units = "&units=metric";
+// const apiKey = "&appid=" + process.env.API_KEY;
+const apiKey = "&appid=" + "f68acfdd8f3c0ea46c672f4d7e1d5d1a";
+
+const getWeather = async (baseUrl, units, key, city) => {
+  const response = await fetch(baseUrl + city + key + units);
+
+  try {
+    const data = await response.json();
+    console.log("getWeather successful", data);
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+//2. Post request to /addData route
+
+const postData = async (url = "", data = {}) => {
+  console.log(data);
+
+  const response = await fetch(url, {
+    method: "Post",
+    credentials: "same-origin",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await response.json();
+    console.log("postDataWorked", newData);
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+//3. get the projectData
+
+const getRecent = async (url = "") => {
+  const request = await fetch(url);
+
+  try {
+    const allData = await request.json();
+    console.log("getRecent successful:", allData);
+  } catch (err) {
+    console.log("error", err);
+  }
+};
+
+//4. Update user interface with new data
+
+let dateField = document.getElementById("date");
+let tempField = document.getElementById("temp");
+let contentField = document.getElementById("content");
+let titleField = document.getElementById("title");
+
+let iconField = document.getElementById("icon");
+let icon = document.createElement("img");
+iconField.appendChild(icon);
+
+const updateUI = async () => {
+  const request = await fetch("/projectData");
+
+  try {
+    const allData = await request.json();
+    console.log("from updateUI:", allData);
+
+    dateField.innerHTML = allData.date;
+    tempField.innerHTML = allData.temp + " °C";
+    contentField.innerHTML =
+      "Expecting " + allData.description + " and feeling " + allData.feeling;
+    titleField.innerHTML = allData.city;
+    icon.src = `http://openweathermap.org/img/wn/${allData.icon}@2x.png`;
+
+    iconField.appendChild(img);
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+// window.onload = () => {
+//   document.getElementById("generate").click();
+// };
